fc8c5f7267e07ed8110c8b0449daa1980dc13433
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2023-04-21 16:41:27 -0400

    Added basic stylings for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
+import '../styles/header.css'
+
 function Header() {
     return (
-        <header>
-            <h1>This is the header</h1>
+        <header className='header'>
+            <img src="/images/icon-menu.svg" alt='menu-icon'></img>
+            <img className='logo' src="/images/logo.svg" alt="logo"></img>
+            <nav className='nav'>
+                <ul className='nav-items'>
+                    <li><a>Collection</a></li>
+                    <li><a>Men</a></li>
+                    <li><a>Women</a></li>
+                    <li><a>Contact</a></li>
+                    <li><a>Contact</a></li>
+                </ul>
+            </nav>
+            <img className='cart' src='/images/icon-cart.svg' alt='cart'></img>
+            <img className='avatar' src='/images/image-avatar.png' alt='avatar'></img>
         </header>
     )
 }

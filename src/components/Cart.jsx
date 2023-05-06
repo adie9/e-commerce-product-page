d33678b48227670eced1c3d62283d41dfb4d5e4a
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2023-05-05 21:12:32 -0400

    Minor styling change

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,15 +1,18 @@
 import '../styles/cart.css'
 
-function CartItems({cart, imageIndex}) {
+function CartItems({cart, setCart, imageIndex}) {
     const thumbnails = ["/images/image-product-1-thumbnail.jpg", 
                         "/images/image-product-2-thumbnail.jpg", 
                         "/images/image-product-3-thumbnail.jpg", 
                         "/images/image-product-4-thumbnail.jpg"];
+
+
     const items = cart.map((item, index) => 
     
         <li className='item-container' key={index}>
             <img className='thumbnail' src={thumbnails[imageIndex]} alt='product'></img>
             <p>Fall Limited Edition Sneakers <br></br> $125.00 x {item} <span><strong>${125 * item}.00</strong></span></p>
+            <img className='delete' src='/images/icon-delete.svg' alt='delete'></img>
         </li>
     );
 
@@ -27,7 +30,7 @@ function Cart({cart, setCart, imageIndex}) {
             <h4 className='cart-title'>Cart</h4>
             <hr></hr>
             <div className="cart-content">
-                {cart.length > 0 ? <CartItems cart={cart} imageIndex={imageIndex} /> : <p className='empty'>Your cart is empty</p>}
+                {cart.length > 0 ? <CartItems cart={cart} setCart={setCart} imageIndex={imageIndex} /> : <p className='empty'>Your cart is empty</p>}
             </div>
 
         </div>
